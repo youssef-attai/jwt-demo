8cbae98e4472fc7d8cb6495b9978ce895118504a
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-04 09:46:29 +0200

    Create the token verification middleware
    Hide the get posts endpoint behind the jwt verification

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ const posts = [
 const app = express();
 app.use(express.json());
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username == req.user.name));
 });
 
 app.post("/login", (req, res) => {
@@ -34,5 +34,18 @@ app.post("/login", (req, res) => {
 
   res.json({ accessToken });
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = data;
+    next();
+  });
+}
 
 app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`));
