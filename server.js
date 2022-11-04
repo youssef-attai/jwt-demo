db7b2119881da3a6e1a9dc1f7f5700f85d7b7405
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-04 09:32:13 +0200

    Create basic endpoints
    - Get posts
    - Login

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 import * as dotenv from "dotenv";
+import * as jwt from "jsonwebtoken";
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 import express from "express";
 
+const posts = [
+  {
+    username: "Youssef",
+    title: "Post 1",
+  },
+  {
+    username: "Omar",
+    title: "Post 2",
+  },
+];
+
 const app = express();
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.post("/login", (req, res) => {
+  // Authenticate User
+
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+  res.json({ accessToken });
+});
 
 app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`));
